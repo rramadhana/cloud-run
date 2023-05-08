983c63562bb4bf70442cfc4e38192e7b04173f1c
rramadhana <119663465+rramadhana@users.noreply.github.com>
2023-05-08 17:21:57 +0700

    s

--- server.js
+++ server.js
@@ -1,89 +1,54 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Connect to MongoDB database
-mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-// Define a mongoose schema for our data
-const Schema = mongoose.Schema;
-const UserSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  age: { type: Number, required: true },
-});
-
-// Define a mongoose model for our data
-const User = mongoose.model('User', UserSchema);
+// Initialize an array to store user data
+let users = [];
 
 // Parse incoming JSON data
 app.use(express.json());
 
 // Define routes for CRUD operations
-app.post('/users', async (req, res) => {
-  try {
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+app.post('/users', (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(201).json(user);
 });
 
-app.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
 app.get('/users/:id', getUser, (req, res) => {
   res.json(res.user);
 });
 
-app.patch('/users/:id', getUser, async (req, res) => {
+app.patch('/users/:id', getUser, (req, res) => {
+  const user = res.user;
   if (req.body.name != null) {
-    res.user.name = req.body.name;
+    user.name = req.body.name;
   }
   if (req.body.email != null) {
-    res.user.email = req.body.email;
+    user.email = req.body.email;
   }
   if (req.body.age != null) {
-    res.user.age = req.body.age;
+    user.age = req.body.age;
   }
-  try {
-    const updatedUser = await res.user.save();
-    res.json(updatedUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
+  res.json(user);
 });
 
-app.delete('/users/:id', getUser, async (req, res) => {
-  try {
-    await res.user.remove();
-    res.json({ message: 'User deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+app.delete('/users/:id', getUser, (req, res) => {
+  const index = users.indexOf(res.user);
+  users.splice(index, 1);
+  res.json({ message: 'User deleted' });
 });
 
 // Middleware function to get a user by ID
-async function getUser(req, res, next) {
-  let user;
-  try {
-    user = await User.findById(req.params.id);
-    if (user == null) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
+function getUser(req, res, next) {
+  const id = req.params.id;
+  const user = users.find(user => user.id === id);
+  if (user == null) {
+    return res.status(404).json({ message: 'User not found' });
   }
   res.user = user;
   next();
